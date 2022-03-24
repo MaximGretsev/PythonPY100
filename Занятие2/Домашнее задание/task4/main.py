f815049b373f815efd32139cca88bf27f796644b
Maxim <m.gretcev@loyaltyplant.com>
2022-03-24 20:59:21 +0300

    Homework task 4. List and indexes.

--- Занятие2/Домашнее задание/task4/main.py
+++ Занятие2/Домашнее задание/task4/main.py
@@ -1,9 +1,25 @@
 if __name__ == "__main__":
-    numbers = []
-    for i in range(0, 11):
-        numbers.append(i * i)
-        last_ = numbers[0]
-        max_ = max(numbers)
-    pass
+    list_ = []
+    for i in range(3, 15):
+        list_.append(i * 2)
+    print(list_)
+    min_index_value = 0
+    min_value = list_[min_index_value]
+    max_index_value = 0
+    max_value = list_[max_index_value]
+    for i in range(len(list_)):
+        current_value = list_[i]
+        if current_value > max_value:
+            max_value = current_value
+            max_index_value = i
+    for i in range(len(list_)):
+        current_min = list_[i]
+        if current_min < min_value:
+            current_min = min_value
+            min_index_value = i
+    list_[min_index_value], list_[max_index_value] = list_[max_index_value], list_[min_index_value]
+    print(list_)
+    print(list_[min_index_value])
+    print(list_[max_index_value])
 
-    print(numbers)
+    pass
