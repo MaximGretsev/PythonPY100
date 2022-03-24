4aff6c104847c3855c36646f288ae0bc1645a194
Maxim <m.gretcev@loyaltyplant.com>
2022-03-24 21:19:35 +0300

    Lab Class 3

--- Занятие3/Лабораторные_задания/task1_2/main.py
+++ Занятие3/Лабораторные_задания/task1_2/main.py
@@ -1,5 +1,9 @@
-# TODO запишите здесь функцию factorial
+def factorial(n):# TODO запишите здесь функцию factorial
+    count = 1
+    for i in range(1, n + 1):
+        count *= i
+    return count
 
 
 if __name__ == "__main__":
-    # TODO распечатать результат выполнения функции factorial от числа 5
+    print(factorial(5))# TODO распечатать результат выполнения функции factorial от числа 5
